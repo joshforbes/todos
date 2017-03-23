bb24e594265a2ad67fe3debb58ff1cace9415abc
Josh Forbes <jforbes@kimmel.com>
2017-03-22 23:12:52 -0400

    Making the app start up cleaner.

--- koa/app.js
+++ koa/app.js
@@ -1,14 +1,13 @@
-'use strict';
+"use strict";
 
-const Koa = require('koa');
+const Koa = require("koa");
 const app = new Koa();
-const router = require('koa-router')();
-const bodyParser = require('koa-bodyparser');
-const todos = require('./routes/todos');
+const router = require("koa-router");
+const bodyParser = require("koa-bodyparser");
+const todos = require("./routes/todos");
 
-app.use(bodyParser());
+const api = router().use("/todos", todos.routes());
 
-router.use('/todos', todos.routes(), todos.allowedMethods());
-app.use(router.routes());
+app.use(bodyParser()).use(api.routes()).use(api.allowedMethods());
 
 app.listen(3000);
